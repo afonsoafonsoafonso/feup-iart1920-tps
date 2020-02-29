dc8d53fe28363db3ffee277ea3ba1dca9a4233b1
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2020-02-29 14:20:31 +0000

    bucket bfs done

--- tp1/buckets_bfs.py
+++ tp1/buckets_bfs.py
@@ -8,69 +8,73 @@ n = int(sys.argv[3])
 initialState = (0,0)
 objectiveState = (n, 0)
 
-queue = []
-queue.append(initialState)
-
-paths = {}
-paths[initialState] = []
+queue = [initialState]
+paths = {initialState: []}
 
 visited = {}
 
-while queue:
+while queue and queue[0]!=objectiveState:
     b1 = queue[0][0]
     b2 = queue[0][1]
-    print("B1:" + str(b1) + " ; " + "B2:" + str(b2))
-
-    if queue[0] == objectiveState:
-        break;
 
     queue.pop(0)
 
-    if (b1, b2) in visited.keys():
-        continue
-    else: visited[(b1, b2)] = True
-
-    if b1 < c1:
+    if b1 < c1 and (c1, b2) not in visited.keys():
         queue.append( (c1, b2) )
-        visited[(c1, b2)] = True
-        paths[(c1, b2)] = paths[(b1, b2)].append((b1, b2))
+        path = paths[(b1, b2)].copy()
+        path.append((b1, b2))
+        paths[(c1, b2)] = path
 
-    if b2 < c2:
+    if b2 < c2 and (b1, c2) not in visited.keys():
         queue.append( (b1, c2) )
-        visited[(b1, c2)] = True
-        paths[(b1, c2)] = paths[(b1, b2)].append((b1, b2))
+        path = paths[(b1, b2)].copy()
+        path.append((b1, b2))
+        paths[(b1, c2)] = path
 
-    if b1 > 0:
+    if b1 > 0 and (0, b2) not in visited.keys():
         queue.append( (0, b2) )
-        visited[(0, b2)] = True
-        paths[(0, b2)] = paths[(b1, b2)].append((b1, b2))
+        path = paths[(b1, b2)].copy()
+        path.append((b1, b2))
+        paths[(0, b2)] = path
 
-    if b2 > 0:
+    if b2 > 0 and (b1, 0) not in visited.keys():
         queue.append( (b1, 0) )
-        visited[(b1, 0)] = True
-        paths[(b1, 0)] = paths[(b1, b2)].append((b1, b2))
+        path = paths[(b1, b2)].copy()
+        path.append((b1, b2))
+        paths[(b1, 0)] = path
 
-    if b1 > 0 and b2 < c2 and b1 > (c2 - b2):
+    if b1 > 0 and b2 < c2 and b1 >= (c2 - b2) and (b1-(c2-b2), c2) not in visited.keys():
         queue.append( (b1-(c2-b2), c2) )
-        visited[(b1-(c2-b2), c2)] = True
-        paths[(b1-(c2-b2), c2)] = paths[(b1, b2)].append((b1, b2))
+        path = paths[(b1, b2)].copy()
+        path.append((b1, b2))
+        paths[(b1-(c2-b2), c2)] = path
 
-    if b1 > 0 and b2 < c2 and b1 < (c2 - b2):
+    if b1 > 0 and b2 < c2 and b1 < (c2 - b2) and (0, b2+b1) not in visited.keys():
         queue.append( (0, b2+b1))
-        visited[(0, b2+b1)] = True
-        paths[(0, b2+b1)] = paths[(b1, b2)].append((b1, b2))
+        path = paths[(b1, b2)].copy()
+        path.append((b1, b2))
+        paths[(0, b2+b1)] = path
 
-    if b2 > 0 and b1 < c1 and b2 > (c1 - b1):
+    if b2 > 0 and b1 < c1 and b2 >= (c1 - b1) and (c1, b2-(c1-b1) ) not in visited.keys():
         queue.append( (c1, b2-(c1-b1)) )
-        visited[(c1, b2-(c1-b1))] = True
-        paths[(c1, b2-(c1-b1))] = paths[(b1, b2)].append((b1, b2))
+        path = paths[(b1, b2)].copy()
+        path.append((b1, b2))
+        paths[(c1, b2-(c1-b1))] = path
 
-    if b2 > 0 and b1 < c1 and b2 < (c1 - b1):
+    if b2 > 0 and b1 < c1 and b2 < (c1 - b1) and (b1+b2, 0) not in visited.keys():
         queue.append( (b1+b2, 0) )
-        visited[(b1+b2, 0)] = True
-        paths[(b1+b2, 0)] = paths[(b1, b2)].append((b1, b2))
+        path = paths[(b1, b2)].copy()
+        path.append((b1, b2))
+        paths[(b1+b2, 0)] = path
+
+    visited[(b1, b2)] = True
 
 # fim
+if objectiveState in visited.keys():
+    print("Path to solution: ")
+    print(paths[objectiveState])
+else: 
+    print("No solution was found.")
 
 
     
